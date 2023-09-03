bcfa837fc54cb3be2a15504b2d1f85cf12c1bbce
happy1511 <140176910+happy1511@users.noreply.github.com>
2023-09-03 13:01:06 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
-  const res = async()=>{
-
-  }
+  const [h,seth] = useState()
+  useEffect(async()=>{
+    await fetch(process.env.SERVER_URL).then((res)=>{
+      seth(res)
+    }).catch(err =>{
+      console.log(err)
+    })
+  })
   return (
     <div>
-      {process.env.SERVER_URL}
+      {h}
     </div>
   );
 }
