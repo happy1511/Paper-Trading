85a5d3895a15ae3e99f15b71bcbd079fbb353f5d
happy.patel <happy.patel@crestinfotech.co.in>
2024-10-18 16:54:40 +0530

    change

--- server/index.js
+++ server/index.js
@@ -1,59 +1,80 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var port = process.env.PORT || 8000;
+const {
+  mostactive,
+  topgainersandloosers,
+  AllTheIndices,
+  HistoricalData,
+  intraday,
+  news,
+  equity,
+  gainers,
+  getEquityHistoricalData,
+  getAllSymbols,
+  marketstatus,
+} = require("./nseapi/allstock");
 
-var express = require('express')
-var app = express()
-var cors = require('cors')
-var port = process.env.PORT || 8000
-const { mostactive,topgainersandloosers, AllTheIndices, HistoricalData, intraday,news, equity, gainers, getEquityHistoricalData, getAllSymbols, marketstatus } = require('./nseapi/allstock')
-
-app.use(cors())
-
-app.get('/', async (req, res) => {
-    console.log('/')
-    res.json(await marketstatus())
+app.use(cors());
+const url = "https://www.nseindia.com/";
+app.get("/", async (req, res) => {
+  fetch(url + "api/marketStatus")
+    .then(async (response) => {
+      console.log(await response.json());
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  res.json(await marketstatus());
 });
 
+app.get("/getAllSymbols", async (req, res) => {
+  res.send(await getAllSymbols());
+});
 
-app.get('/getAllSymbols', async (req, res) => {
-    res.send(await getAllSymbols())
-})
+app.get("/gainersandLoosers/:symbol", async (req, res) => {
+  res.json(await topgainersandloosers(req.params.symbol));
+});
 
+app.get("/equity/:symbol", async (req, res) => {
+  res.send(await equity(req.params.symbol));
+});
 
-app.get('/gainersandLoosers/:symbol', async (req, res) => {
-    res.json(await topgainersandloosers(req.params.symbol))
-})
+app.get("/news", async (req, res) => {
+  res.send(await news());
+});
 
-app.get('/equity/:symbol', async (req, res) => {
-    res.send(await equity(req.params.symbol))
-})
-
-app.get('/news', async (req, res) => {
-    res.send(await news())
-})
-
-app.get('/intraday/:symbol', async (req, res) => {
-    res.send(await intraday(req.params.symbol))
-})
+app.get("/intraday/:symbol", async (req, res) => {
+  res.send(await intraday(req.params.symbol));
+});
 
 function increaseTimeout(req, res, next) {
-    req.setTimeout(180000);
-    next();
+  req.setTimeout(180000);
+  next();
 }
 
-app.get('/historical/:symbol/:start/:end', increaseTimeout, async (req, res) => {
-    res.send(await HistoricalData(req.params.symbol, {
+app.get(
+  "/historical/:symbol/:start/:end",
+  increaseTimeout,
+  async (req, res) => {
+    res.send(
+      await HistoricalData(req.params.symbol, {
         start: new Date(req.params.start),
-        end: new Date(req.params.end)
-    }))
-})
+        end: new Date(req.params.end),
+      })
+    );
+  }
+);
 
-app.get('/Allindices', async (req, res) => {
-    res.json(await AllTheIndices())
-})
+app.get("/Allindices", async (req, res) => {
+  res.json(await AllTheIndices());
+});
 
-app.get('/mostactive/:indexsymbol',async (req,res) => {
-    res.json(await mostactive(req.params.indexsymbol))
-})
+app.get("/mostactive/:indexsymbol", async (req, res) => {
+  res.json(await mostactive(req.params.indexsymbol));
+});
 
 app.listen(port, () => {
-    console.log(`server is listening port ${port}`)
-})
+  console.log(`server is listening port ${port}`);
+});
